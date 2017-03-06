479358bfe0293765d2aa3cb59999740da33899e4
pedrolabrador <pedrolabrador>
2017-03-06 16:37:54 -0430

    Update - minor fixes, ShakerSort

--- Ordenamiento/shaker.cpp
+++ Ordenamiento/shaker.cpp
@@ -1,75 +1,63 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-/*
-DYLAN PREPAS
-MARTES: 11-1
-MIERCO: 10-12
-VIERNE: 11-1
 
-04247231306
+void ShakerSort(int vec[], int n);
+void Print(int v[], int n);
 
-*/
 using namespace std;
-void shakerSort(int vec[],int n);
-void imprimir(int v[],int n);
-int main(int argc, char** argv){
+
+int main(int argc, char** argv) {
 	
-	int v[]={55,23,31,14,16,19,21,10,12,18,24,36,63,84,2};
-	int vec[50000];
+	int v[] = {7, 16, 14, 8, 2};
+	int vec[25];
 	srand(time(NULL));
-	for(int i=0;i<50000;i++) vec[i]=rand()%500000;
+	for(int i=0;i<25;i++) vec[i] = rand()%100+1;
+	 
+	ShakerSort(v,5);
+	Print(v,5);
 	
-	shakerSort(vec,50000);
-	imprimir(vec,50000);
-	//shakerSort(v,15);
-	//imprimir(v,15);	
+	ShakerSort(vec,25);
+	Print(vec,25);	
 	
 	
 	return 0;
 }
 
-void shakerSort(int v[],int n){
-	
-	int inicio=0, final=n-1;
+void ShakerSort(int v[], int n) {
+
+	int inicio = 0, final = n-1;
 	int aux;
-	bool band=false;
-	int cont=0;
-	do{
-		
-		if(!band){
-			for(int i=0;i<final;i++){
-				if(v[i]>v[i+1]){
-					aux=v[i];
-					v[i]=v[i+1];
-					v[i+1]=aux;
-				}
-				if(i==final-1){
-					band=true;
-					final--;
-				}
-			}	
-		}
-		if(band){	   
-			for(int i=final;i>inicio;i--){
-				if(v[i]<v[i-1]){
-					aux=v[i];
-					v[i]=v[i-1];
-					v[i-1]=aux;
-				}
-				if(i-1==inicio){
-					band=false;
-					inicio++;
+	bool band = false;
+	int cont = 0;
+	do {	
+		if(!band) {
+			for(int i = 0;i < final;i++) {
+				if(v[i] > v[i+1]) {
+					aux = v[i];
+					v[i] = v[i+1];
+					v[i+1] = aux;
 				}
 			}
+			band = true;
+			final--;		
 		}
-		
+		if(band) {	   
+			for(int i = final ;i > inicio;i--) {
+				if(v[i] < v[i-1]){
+					aux = v[i];
+					v[i] = v[i-1];
+					v[i-1] = aux;
+				}
+			}
+			band = false;
+			inicio++;		
+		}	
 		cont++;
-	}while(cont<final/2);
-	
+	} while(cont<n/2);	
 }
-void imprimir(int v[],int n){
-	for(int i=0;i<n;i++){
-		cout << v[i]<<" ";
-	}
+
+void Print(int v[], int n){
+	for(int i=0;i<n;i++) cout << v[i]<<" ";
+	cout << endl;
 }
